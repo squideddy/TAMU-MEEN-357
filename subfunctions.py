1385bb19ff6b4a87400f135de967021d94c28f6a
Villarrealkasey0-rgb <villarrealkasey0@gmail.com>
2025-09-03 17:37:18 -0500

    change

--- subfunctions.py
+++ subfunctions.py
@@ -1,18 +1,20 @@
 from dictionary_357 import rover
 
 def get_mass(): # Computes the total mass of the rover. Uses information in the rover dict.
-    mass = rover['wheel_assembly']['wheel']['mass']*6
+    mass = rover['wheel_assembly']['wheel']['mass'] * 6
+    print(mass)
 
 def get_gear_ratio(): #Returns the speed reduction ratio for the speed reducer based on speed_reducer dict.
+    print('hi')
 def tau_dcmotor(): #Returns the motor shaft torque when given motor shaft speed and a dictionary containing
-                #important specifications for the motor.
+    print('hi')           #important specifications for the motor.
 def F_drive(): #Returns the force applied to the rover by the drive system given information about the drive
-                #system (wheel_assembly) and the motor shaft speed.
+    print('hi')                #system (wheel_assembly) and the motor shaft speed.
 def F_gravity(): #Returns the magnitude of the force component acting on the rover in the direction of its
-                #translational motion due to gravity as a function of terrain inclination angle and rover
+    print('hi')                #translational motion due to gravity as a function of terrain inclination angle and rover
                 #properties.
 def F_rolling(): #Returns the magnitude of the force acting on the rover in the direction of its translational
-                #motion due to rolling resistances given the terrain inclination angle, rover properties, and a
+    print('hi)')                #motion due to rolling resistances given the terrain inclination angle, rover properties, and a
                 #rolling resistance coefficient.
 def F_net(): #Returns the magnitude of net force acting on the rover in the direction of its translational
-                #motion.
+    print('hi)')                #motion.
