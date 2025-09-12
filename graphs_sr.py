17b51bcae7cdf9738ca10ba889572f627ff4d5b2
Andrew <andrew13319@gmail.com>
2025-09-12 12:37:20 -0500

    rough draft of graphs_sr

--- graphs_sr.py
+++ graphs_sr.py
@@ -3,3 +3,50 @@ speed, torque, and power of the speed reducer output shaft (the motor shaft now 
 speed reducer; your graphs should be of the torque, speed, and power of the speed reducer output). As
 in the previous case, please label your axes properly.
 """
+import matplotlib.pyplot as plt
+import numpy as np
+import dictionary_357
+import subfunctions as sf
+
+# Get gear ratio from speed reducer
+Ng = sf.get_gear_ratio(dictionary_357.speed_reducer)
+
+## below should be replaced with subfunction call?
+
+# Generate omega_motor (motor shaft speed) values
+omega_motor = np.linspace(0, dictionary_357.motor['speed_noload'], 100)
+
+
+# Calculate motor shaft torque for each omega
+tau_motor = dictionary_357.motor['torque_stall'] * (1 - omega_motor / dictionary_357.motor['speed_noload'])
+tau_motor = np.clip(tau_motor, 0, dictionary_357.motor['torque_stall'])
+
+## above should be replaced with subfunction call?
+
+# Calculate speed reducer output values
+omega_sr = omega_motor / Ng
+tau_sr = tau_motor * Ng
+power_sr = tau_sr * omega_sr
+
+plt.figure(figsize=(8, 10))
+
+# 1. Speed reducer output shaft speed vs. torque
+plt.subplot(3,1,1)
+plt.plot(tau_sr, omega_sr)
+plt.xlabel("Speed Reducer Output Shaft Torque [Nm]")
+plt.ylabel("Speed Reducer Output Shaft Speed [rad/s]")
+
+# 2. Speed reducer output power vs. torque
+plt.subplot(3,1,2)
+plt.plot(tau_sr, power_sr)
+plt.xlabel("Speed Reducer Output Shaft Torque [Nm]")
+plt.ylabel("Speed Reducer Output Power [W]")
+
+# 3. Speed reducer output power vs. speed
+plt.subplot(3,1,3)
+plt.plot(omega_sr, power_sr)
+plt.xlabel("Speed Reducer Output Shaft Speed [rad/s]")
+plt.ylabel("Speed Reducer Output Power [W]")
+
+plt.tight_layout()
+plt.show()
