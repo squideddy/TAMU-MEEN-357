50f8a6b4b0b7dd26ab09c99cea25e039869d88c1
Villarrealkasey0-rgb <villarrealkasey0@gmail.com>
2025-09-13 13:30:02 -0500

    my_terrian_stuff

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -13,3 +13,34 @@ accelerating. This means you must look for the operating point of the motor at w
 acting on the rover is zero. ***
 • Hint: You can use the no-load and stall speeds of the motor to define an initial bracket for the
 root-finding method. Alternatively, provide an open method with any value on this range."""
+import numpy as np
+#import matplotlib.pyplot as plt
+# 1) Import your config (rover/motor/etc.) defensively
+import dictionary_357 as cfg
+from subfunctions import F_net  # your array-oriented function
+
+# Pull objects (and provide sensible defaults/fallbacks)
+rover  = getattr(cfg, 'rover',  None)
+motor  = getattr(cfg, 'motor',  None)
+planet = 3.72  # fallback to Mars g if missing
+Crr    = 0.2
+
+
+
+
+#Bisection
+def find_v_top(theta_deg, tol=1e-5, max_iter= 120):
+    wL = 0.0 # rad/s
+    wU = motor['speed_noload'] # speed_noload in rad/s
+    fL = F_net(wL, theta_deg, rover, planet, Crr)
+    fU = F_net(wU, theta_deg, rover, planet, Crr)
+
+#Main Function for differnt angles 
+def main():
+    slope_array_deg = np.linspace(-10, 35, 25)  # degrees
+    v_max = np.zeros_like(slope_array_deg, dtype=float)
+    for i, th in enumerate(slope_array_deg):
+        v_max[i] = find_v_top(th)
+
+if __name__ == '__main__':
+    main()
