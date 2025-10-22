60a1624b9b7cbdf2e60a20cd4c316e46403d7594
Villarrealkasey0-rgb <villarrealkasey0@gmail.com>
2025-10-22 17:53:55 -0500

    yes

--- Phase 1/subfunctions.py
+++ Phase 1/subfunctions.py
@@ -262,3 +262,6 @@ def motorW(v,rover): # v is 1D array translational velocity, rover is dictionary
     v_arr = np.asarray(v, dtype=float)
     w_motor = (v_arr / r) * Ng   # [rad/s]
     return w_motor if np.ndim(v) == 0 else w_motor
+
+def rover_dynamics(t,y,rover,planet,exepriment):
+    
