7174026b97dd3f83e057d4afe997b8d23aa785e4
Edward li <edwardtheli@gmail.com>
2025-10-27 09:53:00 -0500

    added units to graph

--- Phase 2/subfunctions.py
+++ Phase 2/subfunctions.py
@@ -412,18 +412,7 @@ def simulate_rover(rover, planet, experiment, end_event):
 
     # get experiment data
     # should populate the telemetry dictionary in rover
-    rover['telemetry'] = {
-    "time": [],
-    "completion_time": 0.0,
-    "velocity": [],
-    "position": [],
-    "distance_traveled": 0.0,
-    "max_velocity": 0.0,
-    "average_velocity": 0.0,
-    "power": [],
-    "battery_energy": 0.0,
-    "energy_per_distance": 0.0
-}
+    
     try:
         time_range = np.asarray(experiment['time_range'], dtype=float)
         initial_conditions = np.asarray(experiment['initial_conditions'], dtype=float)
@@ -439,9 +428,10 @@ def simulate_rover(rover, planet, experiment, end_event):
     sol = solve_ivp(fun=lambda t,y: rover_dynamics(t,y,rover,planet,experiment),
                     t_span=(time_range[0], time_range[1]),
                     y0=initial_conditions,
-                    method='RK45',
+                    method='BDF',
                     events=events,
                     max_step=1.0)
+    
     # store telemetry data
     rover['telemetry']['time'] = sol.t
     rover['telemetry']['velocity'] = sol.y[0,:]
