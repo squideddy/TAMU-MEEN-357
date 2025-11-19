75326ba39679d1d0f312ec6800b2b1d173abfdcb
Villarrealkasey0-rgb <villarrealkasey0@gmail.com>
2025-11-19 16:56:19 -0600

    yes

---
+++ Phase 3/study_parachute_size.py
@@ -0,0 +1,4 @@
+#your team has been asked to conduct a study of the impact on EDL performance of changing the parachute
+#size. For this task, you must do two things: (1) create a Python script called
+#study_parachute_size.py that conducts the analysis and generates useful visualizations of the
+#results
